b682826257811caa7f1a95576f01f491a0b8d4f4
Julia Fragale <pastusiakjulia01@gmail.com>
2025-02-02 16:16:40 -0500

    you better work

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
-import CartItem from './CartItem';
+import { CartItem } from './CartItem';
 import { useSelector, useDispatch } from 'react-redux';
 import { addItem } from './CartSlice';
 
