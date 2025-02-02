4fb4071eeaa9d0ff071a6a1af549cc96c6480551
Julia Fragale <pastusiakjulia01@gmail.com>
2025-02-02 15:44:36 -0500

    cart commit2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -292,7 +292,7 @@ function ProductList() {
                             <div className="product-title">{plant.name}</div>
                             <div className="product-cost">{plant.cost}</div>
                             <div className="product-description">{plant.description}</div>
-                            <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>,
+                            <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                         </div>
                         ))}
                     </div>
